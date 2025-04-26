7678214e20faaedd491fa7db14cd80e9538fce2a
unknown <tranngochuyenn1909@gmail.com>
2025-04-26 16:03:50 +0700

    feat(ui): navigation bar

--- tranngochuyen/src/pages/Home.jsx
+++ tranngochuyen/src/pages/Home.jsx
@@ -3,15 +3,17 @@ import { Input, Carousel, Image } from "antd"
 import { TopBar } from "../components/TopBar"
 import { Header } from "../components/Header"
 import Footer from "../components/Footer"
+import Navigation from "../components/Navigation"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Top Bar */}
       <TopBar/>
 
-      {/* Header */}
       <Header/>
+
+      <Navigation/>
+
 
       <Footer/>
     </div>
